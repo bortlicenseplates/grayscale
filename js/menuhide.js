ad025958f78dc5b873cfbc2810fe17524f396b88
tropical-entropic <jamesryan864@gmail.com>
2017-03-10 10:51:06 +0000

    interview layout fixed

--- js/menuhide.js
+++ js/menuhide.js
@@ -1,14 +1,12 @@
 $(function(){
     $('#dropdown').click(function(){
-        
-          $( "#menu-show" ).slideToggle( "slow", function() {
+      $( "#menu-show" ).slideToggle( "slow", function() {
 		    // Animation complete.
 		  });
     });
 
-    $('.interview').click(function(){
-        
-          $( "#interview-content" ).slideToggle( "fast", function() {
+    $('.moreclick').click(function(){
+      $( "#interview-content" ).slideToggle( "fast", function() {
 		    // Animation complete.
 		  });
     });
